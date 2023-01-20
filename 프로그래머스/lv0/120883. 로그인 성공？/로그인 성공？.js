d01df0a1ba2fae9a7f50d43fdea718de43140d29
dding-v <115635503+dding-v@users.noreply.github.com>
2023-01-20 11:43:54 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,11 +1,5 @@
 function solution(id_pw, db) {
-    let ans = "";
-    db.forEach(el => {
-        if (el[0] === id_pw[0]) {
-            if (el[1] === id_pw[1]) {
-                return ans = "login";
-            } else return ans = "wrong pw";
-        }
-    });
-    return ans ? ans : "fail";
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  return map.has(id) ? (map.get(id) === pw ? 'login' : 'wrong pw') : 'fail';
 }
