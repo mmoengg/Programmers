a1aaab3fb712bf91140222c59864bd29c819a6bb
dding-v <115635503+dding-v@users.noreply.github.com>
2023-01-20 12:56:37 +0900

    [level 0] Title: 문자열 밀기, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,3 +1,14 @@
 function solution(A, B) {
-    return (B + B).indexOf(A)
+    let count = 0;
+
+    const a = A.split("")
+    const b = B.split("")
+
+    while(a.join("") !== b.join("")){
+        a.unshift(a[a.length-1])
+        a.pop()
+        count += 1
+        if(count === a.length) break
+    }
+    return count === a.length ? -1 : count;
 }
